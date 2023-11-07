42c327d7efb1cd550b589b85c9c2557953e93b49
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-11-07 12:56:13 -0500

    zxc

--- main.ts
+++ main.ts
@@ -1,8 +1,49 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Angelo Yalung
+ * Created on: Nov 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let loopCounter = 0
+let sprite: game.LedSprite = null
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// on a button press
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  basic.pause(500)
+  loopCounter = 0
+  sprite = game.createSprite(0, 0)
+
+// the loop for the A button
+  while (loopCounter <= 5) {
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    basic.pause(500)
+    loopCounter = loopCounter + 1
+  }
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
+})
+
+// b button pressed
+input.onButtonPressed(Button.B, function () {
+  basic.clearScreen()
+  basic.pause(500)
+  loopCounter = 5
+  sprite = game.createSprite(0, 0)
+
+// looping for B button
+  while (loopCounter >= 0) {
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    basic.pause(500)
+    loopCounter = loopCounter - 1
+  }
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
+})
